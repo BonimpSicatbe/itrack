fbad61266ad90b41ddb6c5c588ffcea362dded6a
Tan <tanprincessmaejauod@gmail.com>
2025-07-01 23:24:42 +0800

    tan - july 01

--- resources/views/livewire/user/pending-task/calendar-button.blade.php
+++ resources/views/livewire/user/pending-task/calendar-button.blade.php
@@ -1,35 +1,39 @@
 <div class="relative" x-data="{
     showCalendar: false,
+    calendar: null,
     init() {
-        // Watch for Livewire updates
-        Livewire.on('requirementsUpdated', () => {
-            if (this.calendar) {
-                this.calendar.refetchEvents();
+        // Initialize only when fully shown
+        this.$watch('showCalendar', (value) => {
+            if (value && !this.calendar) {
+                this.$nextTick(() => {
+                    this.initCalendar();
+                });
             }
         });
     },
     initCalendar() {
-        if (this.showCalendar && !this.calendar) {
-            this.calendar = new FullCalendar.Calendar(this.$refs.calendar, {
-                plugins: [FullCalendar.dayGridPlugin],
-                initialView: 'dayGridMonth',
-                height: 'auto',
-                headerToolbar: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: ''
-                },
-                events: @js($requirements),
-                eventClick: (info) => {
-                    info.jsEvent.preventDefault();
-                    const event = info.event;
-                    alert(`Requirement: ${event.title}\nDue: ${event.startStr}`);
-                }
-            });
-            this.calendar.render();
-        }
+        this.calendar = new FullCalendar.Calendar(this.$refs.calendar, {
+            plugins: [FullCalendar.dayGridPlugin],
+            initialView: 'dayGridMonth',
+            height: 'auto',
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: ''
+            },
+            events: @js($requirements),
+            eventClick: (info) => {
+                info.jsEvent.preventDefault();
+                const event = info.event;
+                alert(`Requirement: ${event.title}\nDue: ${event.startStr}`);
+            }
+        });
+        this.calendar.render();
+        // Force resize after render
+        setTimeout(() => this.calendar.updateSize(), 100);
     }
 }">
+
     <button 
         @click="showCalendar = !showCalendar; initCalendar()"
         class="btn btn-sm btn-ghost gap-2"
@@ -41,10 +45,13 @@
     <div 
         x-show="showCalendar"
         x-transition
-        class="absolute right-0 mt-2 w-80 bg-white rounded-lg shadow-lg z-10 p-4"
+        class="absolute right-0 mt-2 w-[600px] bg-white rounded-lg shadow-lg z-10 p-4 overflow-hidden"
         x-on:click.outside="showCalendar = false"
         wire:ignore
+        style="max-height: 80vh;"
     >
-        <div x-ref="calendar"></div>
+        <div class="h-full flex flex-col">
+            <div x-ref="calendar" class="flex-grow min-h-0"></div>
+        </div>
     </div>
 </div>
