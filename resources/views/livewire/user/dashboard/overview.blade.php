0e1303335f5f30702bd3ee675e8880f10668f28d
Tan <tanprincessmaejauod@gmail.com>
2025-07-09 13:01:26 +0800

    tan - inayos ko yung sa overview

--- resources/views/livewire/user/dashboard/overview.blade.php
+++ resources/views/livewire/user/dashboard/overview.blade.php
@@ -1,25 +1,21 @@
-<div class="w-full bg-white stats stats-vertical sm:stats-horizontal">
+<div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-3 p-2">
     @foreach ($stats as $stat)
-        <div class="stat w-full">
-            <div class="stat-figure text-2xl">
-                <i class="fa-solid {{ $stat['icon'] }} text-{{ $stat['color'] }}"></i>
-            </div>
-            <div class="stat-title">
-                <span>{{ $stat['title'] }}</span>
-            </div>
-            <div class="stat-value text-{{ $stat['color'] }}">{{ $stat['count'] }}</div>
-            <div class="stat-desc lowercase">
-                @if ($stat['desc'] >= 1024)
-                    {{ number_format($stat['desc'] / 1024, 2) }} GB
-                @else
-                    {{ $stat['desc'] }} MB
-                @endif
-                of
-                @if ($totalFiles >= 1024)
-                    {{ number_format($totalFiles / 1024, 2) }} GB
-                @else
-                    {{ $totalFiles }} MB
-                @endif
+        <div class="flex flex-col p-3 rounded-lg shadow-sm hover:shadow transition-all duration-200 {{ $stat['bg_color'] }}">
+            <div class="flex items-center justify-between">
+                <div>
+                    <p class="text-xs font-medium text-gray-500">{{ $stat['title'] }}</p>
+                    <p class="text-lg font-bold mt-1">
+                        @if(isset($stat['is_total']))
+                            {{ $stat['desc'] }}MB
+                        @else
+                            {{ $stat['count'] }}
+                        @endif
+                    </p>
+                    <p class="text-xs text-gray-500 mt-0.5">{{ $stat['desc'] }}MB</p>
+                </div>
+                <div class="p-2 rounded-md {{ $stat['bg_color'] }}">
+                    <i class="fa-solid {{ $stat['icon'] }} {{ $stat['icon_color'] }}"></i>
+                </div>
             </div>
         </div>
     @endforeach
