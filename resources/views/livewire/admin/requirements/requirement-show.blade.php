67d634b97a95fffaea497e574790a07394d2db10
BonimpSicatbe <101862322+BonimpSicatbe@users.noreply.github.com>
2025-07-23 10:46:09 +0800

    --allow-empty-message

--- resources/views/livewire/admin/requirements/requirement-show.blade.php
+++ resources/views/livewire/admin/requirements/requirement-show.blade.php
@@ -2,48 +2,48 @@
     {{-- requirement details --}}
     <div class="w-full bg-white shadow-md rounded-lg p-6 space-y-4">
         <h2 class="text-xl font-bold">Requirement Details</h2>
-        <dl class="grid grid-cols-3 gap-x-8 divide-gray-500 divide-2">
-            <div class="py-2 flex justify-between col-span-3">
+        <dl class="grid grid-cols-1 md:grid-cols-3 gap-x-8 divide-gray-500 divide-2">
+            <div class="py-2 flex flex-col md:flex-row justify-between col-span-1 md:col-span-3">
                 <dt class="font-bold uppercase">Name</dt>
                 <dd>{{ $requirement->name }}</dd>
             </div>
-            <div class="py-2 flex justify-between col-span-3">
+            <div class="py-2 flex flex-col md:flex-row justify-between col-span-1 md:col-span-3">
                 <dt class="font-bold uppercase">Description</dt>
                 <dd>{{ $requirement->description ?? '-' }}</dd>
             </div>
-            <div class="py-2 flex justify-between">
+            <div class="py-2 flex flex-col md:flex-row justify-between">
                 <dt class="font-bold uppercase">Due Date</dt>
                 <dd>{{ $requirement->due->format('d/m/Y h:i a') }}</dd>
             </div>
-            <div class="py-2 flex justify-between">
+            <div class="py-2 flex flex-col md:flex-row justify-between">
                 <dt class="font-bold uppercase">Assigned To</dt>
                 <dd>{{ $requirement->assigned_to }}</dd>
             </div>
-            <div class="py-2 flex justify-between">
+            <div class="py-2 flex flex-col md:flex-row justify-between">
                 <dt class="font-bold uppercase">Status</dt>
                 <dd>{{ $requirement->status }}</dd>
             </div>
-            <div class="py-2 flex justify-between">
+            <div class="py-2 flex flex-col md:flex-row justify-between">
                 <dt class="font-bold uppercase">Priority</dt>
                 <dd>{{ $requirement->priority }}</dd>
             </div>
-            <div class="py-2 flex justify-between">
+            <div class="py-2 flex flex-col md:flex-row justify-between">
                 <dt class="font-bold uppercase">Created By</dt>
                 <dd>{{ $requirement->createdBy->full_name }}</dd>
             </div>
-            <div class="py-2 flex justify-between">
+            <div class="py-2 flex flex-col md:flex-row justify-between">
                 <dt class="font-bold uppercase">Updated By</dt>
                 <dd>{{ $requirement->updatedBy->full_name ?? '-' }}</dd>
             </div>
-            <div class="py-2 flex justify-between">
+            <div class="py-2 flex flex-col md:flex-row justify-between">
                 <dt class="font-bold uppercase">Archived By</dt>
                 <dd>{{ $requirement->archived_by ?? '-' }}</dd>
             </div>
-            <div class="py-2 flex justify-between">
+            <div class="py-2 flex flex-col md:flex-row justify-between">
                 <dt class="font-bold uppercase">Created At</dt>
                 <dd>{{ $requirement->created_at->format('d/m/Y h:i a') }}</dd>
             </div>
-            <div class="py-2 flex justify-between">
+            <div class="py-2 flex flex-col md:flex-row justify-between">
                 <dt class="font-bold uppercase">Updated At</dt>
                 <dd>{{ $requirement->updated_at->format('d/m/Y h:i a') }}</dd>
             </div>
@@ -52,15 +52,15 @@
 
     {{-- requirement required files --}}
     <div class="w-full bg-white shadow-md rounded-lg p-6 space-y-4">
-        <div class="flex flex-row items-center justify-between w-full gap-4">
+        <div class="flex flex-col md:flex-row items-center justify-between w-full gap-4">
             <h2 class="text-xl font-bold">Requirement Required Files</h2>
             <label for="upload_required_files_modal" class="btn btn-sm btn-ghost btn-default">
                 <i class="fa-solid fa-upload"></i>
                 <span>Upload</span>
             </label>
         </div>
-        <div class=" max-h-[500px] overflow-y-auto">
-            <table class="table table-fixed table-sm table-striped">
+        <div class="max-h-[500px] overflow-x-auto">
+            <table class="table table-fixed table-sm table-striped min-w-[500px]">
                 <thead>
                     <tr class="bg-gray-200">
                         <th>File Name</th>
@@ -82,7 +82,7 @@
                                     class="text-green-500 hover:text-green-700 hover:link">view</button>
                                 <button wire:click='downloadFile({{ $file->id }})' type="button"
                                     class="text-blue-500 hover:text-blue-700 hover:link">download</button>
-                                <button type="button"
+                                <button wire:click='removeFile({{ $file->id }})' type="button"
                                     class="text-red-500 hover:text-red-700 hover:link">remove</button>
                             </td>
                         </tr>
@@ -99,8 +99,8 @@
     {{-- requirement assigned users --}}
     <div class="w-full bg-white shadow-md rounded-lg p-6 space-y-4">
         <h2 class="text-xl font-bold">Requirement Assigned Users</h2>
-        <div class=" max-h-[500px] overflow-y-auto">
-            <table class="table table-fixed table-sm table-striped">
+        <div class="max-h-[500px] overflow-x-auto">
+            <table class="table table-fixed table-sm table-striped min-w-[600px]">
                 <thead>
                     <tr class="bg-gray-200">
                         <th>Name</th>
@@ -130,8 +130,8 @@
     {{-- requirement submissions --}}
     <div class="w-full bg-white shadow-md rounded-lg p-6 space-y-4">
         <h2 class="text-xl font-bold">Requirement Submissions</h2>
-        <div class=" max-h-[500px] overflow-y-auto">
-            <table class="table table-fixed table-sm table-striped">
+        <div class="max-h-[500px] overflow-x-auto">
+            <table class="table table-fixed table-sm table-striped min-w-[700px]">
                 <thead>
                     <tr class="bg-gray-200">
                         <th>File Name</th>
@@ -144,7 +144,6 @@
                 </thead>
                 <tbody>
                     {{-- @forelse ($collection as $item) --}}
-
                     {{-- @empty --}}
                     <tr>
                         <td colspan="6" class="text-center">No files submitted to this requirement.</td>
@@ -171,5 +170,4 @@
     </div>
 
     {{-- view file modal --}}
-
 </div>
